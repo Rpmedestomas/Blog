0dda46020a14e162608820e81a028546b2f243b6
Rpmedestomas <rpmedestomas@gmail.com>
2022-02-27 00:09:38 +0800

    Completed Comments for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :update, :edit, :destroy]
 
     def index
         @posts = Post.all
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def new
@@ -23,12 +23,9 @@ class PostsController < ApplicationController
     end
 
     def edit
-        @post = Post.find(params[:id])
     end
 
     def update
-        @post = Post.find(params[:id])
-
         if @post.update(post_params)
             redirect_to @post
         else
@@ -37,7 +34,6 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
 
         redirect_to root_path
@@ -47,4 +43,10 @@ class PostsController < ApplicationController
     def post_params
         params.require(:post).permit(:title, :content)
     end
+
+    def find_post
+        @post = Post.find(params[:id])
+    end
+
+
 end
